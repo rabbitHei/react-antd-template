40c7d26f0ebb0ff10448146ce5f72538ee997a38
zhoudaofan <daofan.zhou@ximalaya.com>
2019-08-27 17:35:16 +0800

    update common css

--- src/index.js
+++ src/index.js
@@ -3,13 +3,11 @@ import ReactDOM from 'react-dom';
 import Routes from './routes';
 import './index.scss';
 import './utils/axiosMiddleware';
-import { BrowserRouter as Router } from 'react-router-dom';
+// import { BrowserRouter as Router } from 'react-router-dom';
 import * as serviceWorker from './serviceWorker';
 
 ReactDOM.render(
-  <Router basename="/">
-    <Routes />
-  </Router>,
+  <Routes />,
   document.getElementById('root')
 );
 
