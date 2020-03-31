124439c96bd00febdc41e94ec1611a5ccaec1f31
zhoudaofan <daofan.zhou@ximalaya.com>
2020-03-31 18:33:01 +0800

    update uitls

--- src/utils/index.js
+++ src/utils/index.js
@@ -1,4 +1,6 @@
 import axios from 'axios';
+
+// 常用数据计算 & SDK功能
 export const Http = {
     get: ({url,data}) => {
         return new Promise((resolve, reject) => {
@@ -52,3 +54,148 @@ export const Http = {
           })
     }
 }
+
+export const toast = {
+  error: (txt,time)=>{
+      let style = 'display: inline-block;max-width:228px;background: rgb(0,0,0);color:#EA6347;font-size:14px;opacity:0.8;line-height:20px;padding: 10px 20px;border-radius: 5px;box-shadow: 0 4px 12px rgba(0,0,0,0.15); pointer-events: all;'
+      toast.toast(txt,time,style);
+  },
+  info: (txt,time)=>{
+      let style = 'display: inline-block;background: rgb(0,0,0);color:#fff;font-size:14px;opacity:0.8;line-height:20px;padding: 10px 20px;border-radius: 5px;box-shadow: 0 4px 12px rgba(0,0,0,0.15); pointer-events: all;'
+      toast.toast(txt,time,style);
+  },
+  suc: (txt,time)=>{
+      let style = 'display: inline-block;background: rgb(0,0,0);color:#fff;font-size:14px;opacity:0.8;line-height:20px;padding: 10px 20px;border-radius: 5px;box-shadow: 0 4px 12px rgba(0,0,0,0.15); pointer-events: all;'
+      toast.toast(txt,time,style)
+  },
+  updateHtml: (txt,time) => {
+      let style = 'display: inline-block;background: rgb(0,0,0);color:#fff;font-size:14px;opacity:0;line-height:20px;padding: 10px 20px;border-radius: 5px;box-shadow: 0 4px 12px rgba(0,0,0,0.15); pointer-events: all;'
+      toast.toast(txt,time,style)
+  },
+  toast: (txt,time,style)=>{
+      let body = document.querySelector('body');
+      let p = document.createElement('p');
+      let span = document.createElement('span');
+      // p.style = 'width:100%;position:fixed;top:50%;left:0;transform: translateY(-50%);height:40px;display:flex;flex-direction: row;justify-content: center;align-items: center;z-index:9999'
+      p.setAttribute('style','width:100%;position:fixed;top:50%;left:0;transform: translateY(-50%);height:40px;display:flex;flex-direction: row;justify-content: center;align-items: center;z-index:9999')
+      span.innerText = txt;
+      // span.style = style; //兼容低版本不能这么写
+      span.setAttribute('style',style);
+      p.appendChild(span)
+      body.appendChild(p);
+      let timer = setTimeout(()=>{
+          clearTimeout(timer);
+          body.removeChild(p);
+      },time||1600)
+  },
+} 
+
+export const timeToString = (time, format) => {//时间转化
+  let t = new Date(time);
+  let tf = function (i) { return (i < 10 ? '0' : '') + i };
+  let tfNoZroe = function (i) { return i + '' };
+  return format.replace(/yyyy|MM|M|dd|HH|mm|ss/g, function (a) {
+    switch (a) {
+      case 'yyyy':
+        return tf(t.getFullYear());
+        break;
+      case 'MM':
+        return tf(t.getMonth() + 1);
+        break;
+      case 'M':
+        return tfNoZroe(t.getMonth() + 1);
+        break;
+      case 'mm':
+        return tf(t.getMinutes());
+        break;
+      case 'dd':
+        return tf(t.getDate());
+        break;
+      case 'HH':
+        return tf(t.getHours());
+        break;
+      case 'ss':
+        return tf(t.getSeconds());
+        break;
+    };
+  });
+}
+
+export const numCalc = (num, unit, fixed) => {//数字默认转化
+  //num 传入数 fixed 保留小数位数 unit 单位{} k_del:true 代表删除千的匹配
+  let result = 0;
+  let unitTmp = Object.assign({k: '千',w:'万',kw:'千万',y:'亿'}, unit || {});
+  let fixedTmp = fixed === 0 ? 0 : fixed ? fixed : 1;
+
+  if(num>=0){
+    result = num;
+    if(num<1000){
+      result = num
+    }else if(num<10000) {//千
+      if(unitTmp.k_del)return num;
+      result = ratioCalc(num/1000,fixedTmp) + unitTmp.k;
+    }else if(num<10000000){//万
+      if(unitTmp.w_del)return num;
+      result = ratioCalc(num/10000,fixedTmp) + unitTmp.w;
+    }else if(num<100000000){//千万
+      if(unitTmp.kw_del)return num;
+      result = ratioCalc(num/10000000,fixedTmp) + unitTmp.kw;
+    }else if(num>=100000000){//亿
+      if(unitTmp.kw_del)return num;
+      result = ratioCalc(num/100000000,fixedTmp) + unitTmp.y;
+    }
+  }  
+  return  result;
+}
+
+export const ratioCalc=(num,fixed)=>{//保留小数 & 去末尾0处理 1.0 -> 1
+  //num 传入浮点数 fixed 保留小数位数
+  let tmpNum = '0';
+  if(num&&num!==0){
+      num = Math.abs(num);
+      tmpNum =  num+'';
+      let index = (num+'').indexOf('.');
+      if(index>0){
+          let arr = tmpNum.split('');
+          let left = tmpNum.slice(0,index);
+          let right = '';
+          arr.forEach((v,i)=>{
+              if(i>index&&i<=index+fixed){
+                  right = right + v;
+              }
+          });
+          if(right>0){
+              tmpNum = left + '.' + right;
+          }else {
+              tmpNum = left;
+          }
+      }
+  }
+  return tmpNum
+}
+
+export const timeCalc=(time)=>{//计算时间距离现在长久
+  let str  = '';
+  if(+time){
+      let newTime = +time;
+      let nowTime = Date.now();
+      if( (nowTime - newTime) < (60*60*1000) ){//一小时内
+          str = '刚刚'
+      }else if( (nowTime - newTime) < (24*60*60*1000) ) {//一天内
+          str = Math.floor((nowTime - newTime)/(60*60*1000)) + '小时前';
+      }else {//超过一天
+          str = Math.floor((nowTime - newTime)/(24*60*60*1000)) + '天前';
+      }
+  }else {
+      str='刚刚' 
+  }
+  return str;
+}
+
+export const loadJs = (srcEnd)=> {//手动加载JS
+  var script = document.createElement("script")
+  script.type = "text/javascript";
+  script.src = srcEnd;
+  document.getElementsByTagName("body")[0].appendChild(script);
+}
+
